553bac8007cbc1b34fae66efb04f5e09c4ecdb92
Hossain Zahour <apple@vubon.net>
2024-03-22 12:57:16 +0600

    Create LimitChars.php

---
+++ LimitChars.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Traits;
+
+trait LimitChars
+{
+    /* 
+        Review:
+        2024-03-22
+
+        Description:
+        It is a scope function that will limit the characters of the columns.
+        It is basically used for indexing and listing purpose in Laravel.
+
+        @param $columns: array|string
+        @param $limit: integer = 200
+        @return $query
+        
+        Usage:
+        $notices = Notice::latest()
+            ->limitChars(['title', 'message'], 100)
+            ->simplePaginate(10);
+        
+        Author:
+        Md. Hossain Zahour
+        apple@vubon.net
+    */
+    public function scopeLimitChars($query, $columns, $limit = 200)
+    {
+        $sql = '';
+
+        if (is_array($columns)) {
+            // Concating columns
+            foreach ($columns as $column) {
+                $sql .= ", LEFT($column, $limit) as $column";
+            }
+        }
+        else {
+            $sql .= ", LEFT($columns, $limit) as $columns";
+        }
+
+        return $query->selectRaw('*'. $sql);
+    }
+}
